d0ac97f3d8e6ab3e51d03070cb2ee13cf684fba3
weidongge <weidongge@tencent.com>
2023-03-30 18:00:40 +0800

    dev

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+}
